d26efaa232c9c3afc591e9340802e78acc831d99
vick-ram <vickramodero6@gmail.com>
2023-12-10 21:37:04 -0800

    a C file that allocates more memory

---
+++ more_memory.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+/**
+*_realloc - allocates more memory
+*@ptr:- pointer to previously allocated block
+*@old: - old memory block
+*@new: - new memory block
+*Return: - returns nothing
+*/
+void *_realloc(void *ptr, unsigned int old, unsigned int new)
+{
+	char *mem;
+
+	mem = !ptr ? malloc(new) : !new ? (free(ptr), NULL)
+	: new == old ? ptr : malloc(new);
+	if (!mem || mem == ptr)
+		return (mem);
+	old = old < new ? old : new;
+	while(old--)
+		mem[ild] = ((char *)ptr)[old];
+	free(ptr);
+	return (mem);
+}
+/**
+*_ffree - frees string of string
+*@str: - string param
+*Return: - returns nothing
+*/
+void _ffree(char **str)
+{
+	int i;
+
+	if (!str)
+		return;
+	for (i = 0; str[i]; i++)
+		free(str[i]);
+	free(str);
+}
+/**
+*_memset - flls a block of memory with value
+*@ptr: - pointer to memory
+*@b: - byte to fill in memory
+*@size: block size
+*Return: - returns pointer to block of memory
+*/
+char *_memset(char *ptr, char b, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		*(str + i) = b;
+	return (ptr);
+}
